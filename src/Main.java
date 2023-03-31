fc2eb741be6e6c0894a77ebdf9fe07b746ecae9a
Tamer <tamerbilici00@gmail.com>
2023-04-01 01:34:42 +0300

    refactor

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         long now = System.currentTimeMillis();
 
